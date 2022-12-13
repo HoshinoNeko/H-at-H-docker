7319c1accb13b9cbd9ef27387b0ca1c1b297369d
星の猫 <milkycheng@outlook.com>
2022-12-13 17:59:51 +0800

    fix: make go-mmproxy run in subshell

--- build/start.sh
+++ build/start.sh
@@ -22,7 +22,7 @@ ip route delete local 0.0.0.0/0 dev lo table 543
 ip -6 rule delete from ::1/128 iif lo table 543
 ip -6 route delete local ::/0 dev lo table 543
 
-./go-mmproxy -4 127.0.0.1:443 -l 127.0.0.1:3000
+./go-mmproxy -4 127.0.0.1:443 -l 127.0.0.1:3000 &
 
 trap 'kill_jar' TERM INT KILL
 java $JAVA_OPTS -jar HentaiAtHome.jar $HatH_ARGS $HatH_OPTS  &
